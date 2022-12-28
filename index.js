bb1e68bd10ecc4c25ea521aa397e11bdcd94cfab
Abdullah Omer Aidah <a.o.aidah.note9@gmail.com>
2022-12-29 00:36:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(record) {
+    let result;
+    let validator = record.find((record) => record.result === "W")
+    if (validator !== undefined ) {
+        result = validator['year'];
+    }
+    return result;
+}
+
